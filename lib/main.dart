cc7a01bfcbf27cb102d13a1048615aedab718fe6
Zain Fareed <zainfareed093@gmail.com>
2021-04-18 13:51:08 +0500

    column center and divider line

--- lib/main.dart
+++ lib/main.dart
@@ -15,40 +15,45 @@ class MyApp extends StatelessWidget {
         title: Text('MI Card'),
         backgroundColor: Colors.teal,
       ),
-      body: Center(
-        child: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.black,
-                backgroundImage: AssetImage('images/myphoto.jpg'),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.black,
+              backgroundImage: AssetImage('images/myphoto.jpg'),
+            ),
+            Text('Zain Fareed',style: TextStyle(color: Colors.black,fontSize: 30.0,fontStyle: FontStyle.normal)),
+            Text('Flutter Developer', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
               ),
-              Text('Zain Fareed',style: TextStyle(color: Colors.black,fontSize: 30.0,fontStyle: FontStyle.normal)),
-              Text('Flutter Developer', style: TextStyle(color: Colors.black, fontSize: 15.0)),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(Icons.phone,
-                      color: Colors.teal),
-                  title: Text('+92 303 7046121', style: TextStyle(color: Colors.teal)
-                  )
-                ),
-                ),
-              Card(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(Icons.email,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.phone,
                     color: Colors.teal),
-                  title:Text('zfareed@gmail.com', style: TextStyle(color: Colors.teal)
-                  ),
+                title: Text('+92 303 7046121', style: TextStyle(color: Colors.teal)
+                )
+              ),
+              ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.email,
+                  color: Colors.teal),
+                title:Text('zfareed@gmail.com', style: TextStyle(color: Colors.teal)
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       ),
